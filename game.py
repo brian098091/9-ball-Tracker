03fd423d4774c85e3868fd7d64c1aad48c948c27
AKAAka1450 <58277892+AKAAka1450@users.noreply.github.com>
2024-04-09 19:35:01 +0800

    I want to push

--- game.py
+++ game.py
@@ -2,17 +2,27 @@ import numpy as np
 import cv2
 import sys
 import random
+from view import View
 from queue import Queue
 
 class Game():
-    def __init__(self, cap):
-        self.tcrange = [] # table color range
+    def __init__(self, cap, sample_rate=1):
         self.vidcap = cap
-        self.vidlen = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-        self.width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-        self.height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT) 
+        self.sample_rate = sample_rate
+        self.vidlen = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    def set_tcrange_ff(self, img: np.ndarray, loc: tuple, gap:int=1):
+        self.tcrange = [] # table color range
+        self.frame_count = (self.vidlen - 1) // sample_rate + 1
+        self.frames = np.empty(self.frame_count, dtype=object)
+
+
+    def set_tcrange_ff(self, 
+        img: np.ndarray, 
+        loc: tuple, 
+        gap: np.ndarray = np.array([1,1,1], dtype='uint8')
+        ):
         # Find the table color range with Flood Filled algorithm
         # img: source image
         # loc: [y, x] start position
@@ -49,14 +59,15 @@ class Game():
             if c+1 < h and not visited[r][c+1]:
                 visited[r][c+1] = 1
                 que.put((r, c+1))
-        tcrange[0] -= 5 * gap
-        tcrange[1] += 5 * gap
+        tcrange[0] -= np.minimum(tcrange[0], 5 * gap)
+        tcrange[1] += np.minimum(255-tcrange[1], 5 * gap)
         self.tcrange = tcrange
         print('Set tcrange to:', *tcrange, file=sys.stderr)
 
-    def sep_views(self, sample_rate=1):
-        assert 1 <= sample_rate
-        samples = (self.vidlen+sample_rate-1) // sample_rate
+    def sep_views(self, diff_percentage=3):
+        assert 1 <= self.sample_rate
+        samples = self.frame_count
+
         cap = self.vidcap
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
@@ -72,19 +83,17 @@ class Game():
             org[2] += 1
             return org
 
-        max_diff = (3 / 100) * ( self.width * self.height )
-
-        cnttt=0
+        max_diff = (diff_percentage / 100) * ( self.width * self.height )
+        idx = 0
         ret, frame = cap.read()
+        rates = np.empty(self.frame_count, dtype='int32')
         while ret:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             bmask = cv2.inRange(frame, *self.tcrange)
             rate = calc_rate(bmask)
+            rates[idx] = rate
 
-            cnttt+=1
-            if cnttt%1000==0: 
-                print(cnttt, len(views), rate)
-
+            # Update views
             # Binary search for closest rate
             left, right = 0, len(views)
             while left < right:
@@ -94,7 +103,7 @@ class Game():
             cidx = -1 # closest index
             if left >= len(views): cidx = left-1
             elif left-1 < 0: cidx = left
-            else: cidx = left - (abs(views[left-1][0]-rate)<abs(views[left][0]-rate))
+            else: cidx = left - ( abs(views[left-1][0]-rate)<abs(views[left][0]-rate) )
             
             if cidx == -1 or abs(views[cidx][0]-rate)>max_diff:
                 cidx = len(views)
@@ -117,35 +126,47 @@ class Game():
                 views.pop(cidx)
                 cidx -= 1
 
-            for i in range(sample_rate):
+            for i in range(self.sample_rate):
                 ret, frame = cap.read()
+
+            idx += 1
+            if idx % 100==0: 
+                print(idx, len(views), rate)
 
         def filter_view( v ):
             return  v[2] > (2/100) * samples and \
                     v[0] > (2/10) * self.width * self.height
         views = list( filter( filter_view, views) )
+        view_objs = []
         print('Result views:\n', views, file=sys.stderr)
-        for v in views:
-            cv2.imshow(f'{v[0]}', v[1].astype('uint8'))
-        cv2.waitKey()
-        cv2.destroyAllWindows()
+        for i, v in enumerate(views):
+            cv2.imwrite(f'./tmpOutput/view_{i}.jpg', v[1].astype('uint8'))
+            view_objs.append(View(i, v[1]))
+
+        for i, r in enumerate(rates):
+            for j, v in enumerate(views):
+                if abs(r - v[0]) <= max_diff:
+                    self.frames[i] = Frame(i, view_obj[j])
+                    break
+                    
         
         
-j = cv2.imread('resources/test.jpg')
+
+j = cv2.imread('resources/2022_APP_2_000.jpg')
 j = cv2.cvtColor(j, cv2.COLOR_BGR2HSV)
-vidcap = cv2.VideoCapture('./resources/edited.mp4')
-g = Game(vidcap)
-g.set_tcrange_ff(j, (112, 220))
-g.sep_views(50)
-"""
+vidcap = cv2.VideoCapture('./resources/2022_APP_2.mp4')
+g = Game(vidcap, 4)
+g.set_tcrange_ff(j, (520, 520), gap=np.array([10,15,15], dtype='uint8')) # (112, 220)
+print('done')
 # Test by video
-vidcap = cv2.VideoCapture('./resources/edited.mp4')
+
+vidcap = cv2.VideoCapture('./resources/2022_APP_2.mp4')
 ret, frame = vidcap.read()
 width = int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('ffoutput.mp4', fourcc, 20, (width, height))
+out = cv2.VideoWriter('./tmpOutput/ffoutput.mp4', fourcc, 20, (width, height))
 while ret:
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     bin_mask = cv2.inRange(frame, *g.tcrange)
@@ -153,4 +174,28 @@ while ret:
     out.write(img)
     ret, frame = vidcap.read()
 out.release()
-"""
+
+# Test background substraction by video
+vidcap = cv2.VideoCapture('./resources/2022_APP_2.mp4')
+ret, frame = vidcap.read()
+width = int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('./tmpOutput/background_subtraction.mp4', fourcc, 20, (width, height))
+while ret:
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    bin_mask = cv2.inRange(frame, *g.tcrange)
+    res = cv2.bitwise_and(frame, frame, mask=cv2.bitwise_not(bin_mask))
+    # res[np.where(res == [0, 0, 0])] = [230, 100, 100]
+
+    res = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+
+    m = np.all(res[:, :, :3] == [0,0,0], axis=-1)
+    res[m, :3] = [117, 117, 117]
+
+    out.write(res)
+    ret, frame = vidcap.read()
+out.release()
+
+g.sep_views()
---
+++ view.py
@@ -0,0 +1,6 @@
+import cv2
+
+class View:
+    def __init__(self, no, avg_mask):
+        self.view_no = no
+        self.avg_mask = avg_mask
