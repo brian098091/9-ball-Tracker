99f510f725d689ca8e7cb7690595e0cadc6ebe9a
brian098091 <93636904+brian098091@users.noreply.github.com>
2024-06-07 12:42:18 +0800

    circle

--- frame.py
+++ frame.py
@@ -44,6 +44,10 @@ class Frame:
                 cv2.drawContours(output,[box],0,(0,0,255),3) # draws box
                 
             return output
+        
+       
+
+
 
         frame = self.foreground
 
@@ -84,4 +88,6 @@ class Frame:
             detected_objects = cv2.cvtColor(detected_objects, cv2.COLOR_HSV2BGR)
             log.log_image(detected_objects, 'contours')
             #log.log_image(detected_objects_filtered, 'filtered contours')
+            log.save_contour_regions(ctrs, cv2.cvtColor(frame, cv2.COLOR_HSV2BGR))
+        
         return ctrs
--- log_image.py
+++ log_image.py
@@ -1,6 +1,7 @@
 import sys
 import cv2
 import os
+import numpy as np
 
 class Log():
     fcnt = {}
@@ -30,3 +31,35 @@ class Log():
             img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
         cv2.imwrite(fullname, img)
         self.cnter += 1
+    
+    def save_contour_regions(self,ctrs, img, prefix='contour'):
+        for i,ctr in enumerate(ctrs):
+            x, y, w, h = cv2.boundingRect(ctr)
+            roi = img[y:y+h, x:x+w]
+            if w > 30:
+                cv2.imwrite(f'./test_crts/c{str(Log.fcnt[self.folder])}_contour{i}.jpg',roi)
+                GrayImage = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+                GrayImage = cv2.GaussianBlur(GrayImage, (9, 9), 2)
+
+                th = cv2.adaptiveThreshold(GrayImage, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+                
+
+                kernel = np.ones((5, 5), np.uint8)
+                erosion = cv2.erode(th, kernel, iterations=1)
+                dilation = cv2.dilate(erosion, kernel, iterations=1)
+                imgray = cv2.Canny(dilation, 10, 70)
+
+                circles = cv2.HoughCircles(imgray, cv2.HOUGH_GRADIENT, 1, 20,
+                                           param1=50, param2=10, minRadius=5, maxRadius=15)
+                if circles is not None:
+                    circles = np.uint16(np.around(circles))
+                    for circle in circles[0, :]:
+                    # Draw the outer circle
+                        center_x, center_y, radius = circle
+                        original_center_x = x + center_x
+                        original_center_y = y + center_y
+                        cv2.circle(img, (original_center_x, original_center_y), radius, (0, 255, 0), 2)
+                cv2.imwrite(f'./test_crts/c{str(Log.fcnt[self.folder])}_balls.jpg',img)
+        
+        
+        
